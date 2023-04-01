d5677491e5f45baea1ff4c114b22ec7272a894b6
Sezai Ozturk <ozturksezai52@gmail.com>
2023-04-01 14:26:38 +0300

    Feature:mobile menu added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,2 @@
  document.getElementById("copryright-text").innerHTML=`Telif Hakkı ${new Date().getFullYear()} - <b>NİBGAT® | Topluluk</b>`;
+ 
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,2 +1,16 @@
 const activeMenuItem=document.getElementById(window.location.pathname.replace("/", "").split(".")[0]);
 activeMenuItem.style.color="#FF001F";
+const activeMobileMenuItem=document.getElementById(`m-${window.location.pathname.replace("/", "").split(".")[0]}`);
+activeMobileMenuItem.style.color="#FF001F";
+
+const sideMenu=document.getElementById("side-menu");
+var menuState=false;
+function toggleMenu(){
+    menuState=!menuState;
+    if(menuState){
+        sideMenu.classList.add("isActive");
+    }
+    else{
+        sideMenu.classList.remove("isActive");
+    }
+}
